35ae6cd663ce89dba86b9c7308ee5b339b6d0213
kjm <759391013@qq.com>
2020-09-16 00:21:26 +0800

    jsx的基本实现

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-for (const i of [1,2,3]) {
-    console.log(1111)
-}
+import{ createElement,Component,render  } from './toyReact'
 
-let a = <div/>
+class MyComponent extends Component { 
+   render(){
+       return <div>
+          <h1>My component</h1>   
+          { this.children}
+       </div>
+     }
+  }
+
+render (<MyComponent id="wrap" class="test">
+    <div>111</div>
+    <div></div>
+</MyComponent>,document.body);
